7a0e71fb6c4b8e20041a07075a2f4427295baf61
StajMa <60349872+StajMa@users.noreply.github.com>
2020-03-18 13:10:53 +0100

    Add files via upload

---
+++ 2019l4web-webapp-mockup-StajMa-master/pexeso/js.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementsByClassName("board")[0].onclick = onDeskClick;
+    document.getElementsByTagName("html").onclick = onRandomClick;
+    });
+    
+    function onDeskClick(e) {
+        if (e.target.localName === "img")
+            onCardClick(e.target.parentElement.parentElement);
+    }
+    
+    function onCardClick(element) {
+    
+        element.classList.toggle("active");
+    
+        let lastId = "";
+        let isSame = true;
+        let activeCard = document.querySelectorAll(".active");
+        for (let i = 0; i < activeCard.length; i++) {
+            let card = activeCard[i];
+            let cardId = card.dataset.cardtype;
+            if (lastId === "") {
+                lastId = cardId;
+            }
+            else if (cardId !== lastId) { isSame = false; break;}
+        }
+    
+    
+        if (isSame)
+            document.getElementById("shoda").innerText = "Je tam";
+        else
+            document.getElementById("shoda").innerText = "Není tam";
+            
+    }
+    
